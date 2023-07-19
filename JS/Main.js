181c8ea431c7fcbb74997cb418081e8344948f75
MichaelCamacho710 <139907700+MichaelCamacho710@users.noreply.github.com>
2023-07-19 13:53:38 -0500

    Commit Inicial

---
+++ JS/Main.js
@@ -0,0 +1,12 @@
+function suma(){
+    let A = 0;
+    let B = 0;
+    let SUMA = 0;
+    
+    A = parseInt(prompt("Por favor ingrese el primer valor a sumar"));
+    B = parseInt(prompt("Por favor ingrese el primer valor a sumar"));
+
+    SUMA = A + B;
+
+    alert("La suma de los valorees ingresados es: " + SUMA)
+}
